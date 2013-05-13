a1a09d422b9a5b227a6d4353f3a536e28c3a452c
maslo174 <maslo174@gmail.com>
2013-05-13 23:06:02 +0400

    Create string_parser.c
    работает если длина будет меньше 100 слов и длина каждого слова будет не больше 100, опционально можно доработать-надо поколдовать с reallocom

---
+++ shell/shell/string_parser.c
@@ -0,0 +1,83 @@
+char** String_parser (char* string)
+{
+  char** parts=(char**) malloc (100*sizeof(char**));
+	if (parts==NULL)
+	{
+		printf ("error in malloc");
+		return 0;
+	}
+	int i;
+	for (i=0;i<100;++i)
+	{
+		parts[i]=(char*) malloc (5*sizeof(char*));
+		if (parts[i]==NULL)
+		{
+			printf ("error in malloc");
+			return 0;
+		}
+	}
+	int ii;
+	int j=0;//номер буквы в текущем слове
+	int k=0;//номер слова которое мы сейчас заполняем
+	int s=100;//количество слов, есди что, увеличиваем его reallocом
+	int p=100;//размер слова, если что увеличиваем его reallocом
+	for (i=0;i<strlen(string)+1;++i)
+	{	
+		//printf ("strlen=%d\n",strlen(string));
+		//printf ("i=%d\n",i);
+		if (string[i]!=' ' && string[i]!='\n')
+		{
+			if (j<p)
+			{
+				//printf ("next i=%d\n",i);
+				//printf ("k=%d   j=%d\n",k,j);
+				parts[k][j]=string[i];
+				//printf ("%c\n",parts[k][j]);
+			}
+			else
+			{
+				p=p*2;
+				realloc(parts[k],p);
+				parts[k][j]=string[i];
+			}
+			++j;
+		}
+		else
+			if (string[i]==' ')
+			{	
+				//printf("%s\n",parts[k]);
+				realloc(parts[k],j+1);
+				if (parts[k]==NULL)
+				{
+					printf("error in realloc\n");
+					return 0;
+				}
+				parts[k][j]='\0';
+				j=0;
+				k=k+1;
+				if (k==s)
+				{
+					//printf("reallocation\n");
+					s=s*2;
+					realloc(parts,s);
+					{
+						for (ii=k;ii<s'++ii)
+						{
+							parts[ii]=(char*) malloc (100*sizeof(char*));
+						}
+					}
+				//	printf("s=%d\n",s);
+				//	parts[8]="terffdjhgfjd";
+				//	printf ("%s\n",parts[8]);
+				}
+				p=100;
+			}
+	}
+	realloc(parts,k);
+	if (parts==NULL)
+	{
+		printf ("error in realloc");
+		return 0;
+	}
+	return parts;
+}
