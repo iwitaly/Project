e62ba43fb430a1884a29fa08abe1d273c479cb1a
Виталий Давыдов <iwitaly@me.com>
2013-05-13 21:55:35 +0400

    input

--- shell/shell/main.c
+++ shell/shell/main.c
@@ -7,10 +7,40 @@
 //
 
 #include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+char command[200];
+char fullPath[300] = "/bin/";
+char currentPath[500];
+
+void usersInputToTheString(char *str) {
+    gets(str);
+    while (str == NULL || strlen(str) == 0) {
+        printf("input another string\n");
+    }
+}
 
 int main(int argc, const char * argv[])
 {
-
+    printf("%s", argv[0]);
+    while (1) {
+        scanf("%s", command);
+        
+        strcat(fullPath, command);
+        
+        printf("%s\n", fullPath);
+        if (!fork()) {
+            execl(fullPath, command, NULL);
+            
+            perror("");
+        }
+        else {
+            strcpy(fullPath, "/bin/");
+            fullPath[strlen("/bin/")] = '\0';
+            strcpy(command, "");
+        }
+    }
     return 0;
 }
-
